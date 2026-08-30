5c610a6ffe6b14cef8174ea5be0d24e9d11d0609
Shyddaas <shyddaas@gmail.com>
2026-08-30 21:01:24 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -57,7 +57,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://res.cloudinary.com https://lh3.googleusercontent.com https://www.google-analytics.com",
-              "connect-src 'self' https://api.razorpay.com https://www.google-analytics.com https://vitals.vercel-insights.com",
+              "connect-src 'self' https://api.cloudinary.com https://res.cloudinary.com https://api.razorpay.com https://www.google-analytics.com https://vitals.vercel-insights.com",
               "frame-src https://checkout.razorpay.com",
               "frame-ancestors 'none'",
             ].join("; "),
